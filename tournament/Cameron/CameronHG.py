66e1adadde8b97008e14323c3d4466d56dbef0fe
Cameron <57145246+CameronHG@users.noreply.github.com>
2020-03-03 10:10:29 -0600

    rewg

--- tournament/Cameron/CameronHG.py
+++
@@ -1,5 +0,0 @@
-import random
-
-team_name = 'Cringe-Ass Nae Nae Baby'
-strategy_name = 'Massive Cash Prizes'
-strategy_description = 'Get the bag'
---
+++ tournament/CameronHG.py
@@ -0,0 +1,8 @@
+import random
+
+team_name = 'Massive Cash Prizes'
+strategy_name = 'Massive Cash Prizes'
+strategy_description = 'Get the bag'
+
+
+def move(my_history, their_history, my_score, their_score):
