c5ba8c9f5efd711bceb5f73f4a0c3ee1b7f43c34
Cameron <57145246+CameronHG@users.noreply.github.com>
2020-03-03 10:15:57 -0600

    Update CameronHG.py

--- tournament/CameronHG.py
+++ tournament/CameronHG.py
@@ -1,8 +1,8 @@
 import random
 
 team_name = 'Massive Cash Prizes'
-strategy_name = 'Massive Cash Prizes'
+strategy_name = 'Reactive'
 strategy_description = 'Get the bag'
 
 
-def move(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score, opponent_name):
