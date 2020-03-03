dcd3d1cae2a5589a14cbfbe1479ec7e2dbcef583
Cameron <57145246+CameronHG@users.noreply.github.com>
2020-03-03 10:24:30 -0600

    whoops

--- tournament/CameronHG.py
+++
@@ -1,8 +0,0 @@
-import random
-
-team_name = 'MassiveCashPrizes'
-strategy_name = 'Reactive'
-strategy_description = 'Get the bag'
-
-
-def move(my_history, their_history, my_score, their_score, opponent_name):
