f3ff12f972a4a99c19fb451420e871dad14af295
lilliananderso <57363174+lilliananderso@users.noreply.github.com>
2020-03-04 10:55:14 -0600

    gi thub

--- examplemodules/HenryLillianDilemma.py
+++ examplemodules/HenryLillianDilemma.py
@@ -8,14 +8,15 @@
 
 team_name = 'Henry and Lillian'
 strategy_name = 'our strategy is winning'
-strategy_description = """\
-Collude first round. Compare all rounds to the previous round and 
-assume opponent will behave the same as the first time the previous 
-round's result occurred. If the previous round's result never has 
-happened, collude except after being severly punished."""
+strategy_description =
 
 
-def move(my_history, their_history, my_score, their_score):
+
+
+import random
+
+
+def move(my_history, their_history, my_score, their_score, opponent_name):
     """Make my move based on the history with this player.
 
     history: a string with one letter (c or b) per round that has been played with this opponent.
@@ -25,29 +26,14 @@ def move(my_history, their_history, my_score, their_score):
 
     Returns 'c' or 'b' for collude or betray.
     """
-    if len(my_history) == 0:  # It's the first round: collude
-        return 'c'
+    # If the other player has betrayed or this is the last half of the game,
+    if 'b' in their_history or len(their_history) > 100:
+        return 'b'               # Betray.
     else:
-        # If there was a previous round just like the last one,
-        # do whatever they did in the round that followed it
+        return 'c'         # but 90% of the time collude
 
-        # Reference last round
-        recent_round_them = their_history[-1]
-        recent_round_me = my_history[-1]
 
-        # Look at rounds before that one
-        for round in range(len(my_history) - 1):
-            prior_round_them = their_history[round]
-            prior_round_me = my_history[round]
-            # If one matches
-            if (prior_round_me == recent_round_me) and \
-                    (prior_round_them == recent_round_them):
-                return their_history[round]
-        # No match found
-        if my_history[-1] == 'c' and their_history[-1] == 'b':
-            return 'b'  # Betray if they were severely punished last time
-        else:
-            return 'c'  # Otherwise collude.
+
 
 
 
