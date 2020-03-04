dbcccf09fb3a51f74823be07defc57bc9bbd7048
nnar2201 <57227967+nnar2201@users.noreply.github.com>
2020-03-04 11:08:57 -0600

    Create nayanwilllose.py

---
+++ examplemodules/nayanwilllose.py
@@ -0,0 +1,12 @@
+team_name = 'nayanwilllose'
+strategy_name = 'Try not to lose'
+strategy_description = "Do not lose"
+
+def move(my_history, their_history, my_score, their_score):
+    if my_score < -700:
+        return 'c'
+    elif len(their_history) > 0 and 'b' in their_history[-1]:
+        return 'b'
+    elif
+
+
