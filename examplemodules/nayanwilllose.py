9ee12a25565f650b0aa951185f54cd9ce84cb892
nnar2201 <57227967+nnar2201@users.noreply.github.com>
2020-03-04 11:13:10 -0600

    Update nayanwilllose.py

--- examplemodules/nayanwilllose.py
+++ examplemodules/nayanwilllose.py
@@ -2,11 +2,13 @@ team_name = 'nayanwilllose'
 strategy_name = 'Try not to lose'
 strategy_description = "Do not lose"
 
-def move(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score, opponent_name):
     if my_score < -700:
         return 'c'
     elif len(their_history) > 0 and 'b' in their_history[-1]:
         return 'b'
-    elif
+    elif my_score < -1000 and 'b' in their_history[-1]:
+        return 'b'
+
 
 
