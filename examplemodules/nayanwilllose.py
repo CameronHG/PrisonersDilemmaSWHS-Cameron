4824d9beaabc5aa6a6a3d33580c571fddd5cb1a0
nnar2201 <57227967+nnar2201@users.noreply.github.com>
2020-03-11 11:46:32 -0500

    Update nayanwilllose.py

--- examplemodules/nayanwilllose.py
+++ examplemodules/nayanwilllose.py
@@ -4,7 +4,7 @@ strategy_description = "Do not lose"
 
 def move(my_history, their_history, my_score, their_score, opponent_name):
     if my_score < -700:
-        return 'c'
+        return 'b'
     elif len(their_history) > 0 and 'b' in their_history[-1]:
         return 'b'
     elif my_score < -1000 and 'b' in their_history[-1]:
@@ -21,14 +21,20 @@ def move(my_history, their_history, my_score, their_score, opponent_name):
         if (prior_round_me == shark) and \
                 (prior_round_them == sharks):
             return 'c'
-    elif '5' or '6' or '4' in opponent_name and their_history[-1] == 'b':
-        return 'c'
     elif their_score < -400 and their_score > -750:
         return 'c'
-    elif 'Ethan' in opponent_name and len(their_history) > 5 and their_history[-1] == 'b':
+    elif 'Ethan' in opponent_name and len(their_history) > 5 and their_history[-1]  == 'b' or my_history == '6':
         return 'b'
-    elif 'HHHHHHH' in opponent_name and my_history[-1] == 'c':
+    elif 'HHHHHHH' in opponent_name and my_history[-1] == 'b':
         return 'b'
+    elif 'Henry' in opponent_name:
+        return 'b'
+    elif 'Test' in opponent_name and 'b' in my_history[-4]:
+        return 'b'
+    elif 'Huge' in opponent_name and len(their_history) > 1 and my_history[-1] == 'c':
+        return 'b'
+    else:
+        return 'c'
 
 
 
