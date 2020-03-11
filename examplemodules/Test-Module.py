cbf03ec86517ced92f5eabc6037121e800d308e7
Binder42 <57228018+Binder42@users.noreply.github.com>
2020-03-11 11:30:20 -0500

    Update Test-Module.py

--- examplemodules/Test-Module.py
+++ examplemodules/Test-Module.py
@@ -1,3 +1,10 @@
+
+import GLOBALS
+
+c = GLOBALS.COLLUDE
+
+b = GLOBALS.BETRAY
+
 team_name='Test_Module'
 strategy_name='Test_Strategy'
 Strategy_description='''k'''
@@ -5,4 +12,10 @@ Strategy_description='''k'''
 
 def move(my_history, their_history, my_score, their_score):
 
-if 'b' in 
+if 'b' in their_history[-4:]:
+    return 'b'
+else:
+    if random.number() < 0.2
+        return 'b'
+    else:
+        return 'c'
